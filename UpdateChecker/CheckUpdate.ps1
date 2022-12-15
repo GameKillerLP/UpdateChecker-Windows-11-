13b17355a9e9a02b66010ebac746ad44130c9a93
ZombiexS <63654379+GameKillerLP@users.noreply.github.com>
2022-12-15 17:26:11 +0100

    Add files via upload

---
+++ UpdateChecker/CheckUpdate.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module WindowsUpdate
+
+#Search for Windows Updates and Install them
+Get-WindowsUpdate 
+Install-WindowsUpdate -AcceptAll
+
+# Get Date Variable
+$timestamp = Get-Date -Format "HH.mm.ss.dd.MM.yyyy"
+
+# Create a .txt file In C:\Windows\Logs\Updates
+Write-Output (Get-WindowsUpdateLog) (Get-WindowsUpdate)| Out-File C:\UpdateChecker\logs\UpdateLog_$timestamp.txt
